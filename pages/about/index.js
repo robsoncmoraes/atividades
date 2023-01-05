888b0376a12eae4e3039cacd9075f7045102a1f1
robsoncmoraes <robsonmoraes@outlook.com>
2023-01-05 12:10:38 -0300

    primeira tarefa

---
+++ pages/about/index.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+export default function About(){
+    return (
+    <>
+    <h1>Sobre</h1>
+    <Link href="/">Inicio</Link>
+    <br/>
+    <h2>Informação</h2>
+    </>
+    )
+
+}
---
+++ pages/products/index.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+export default function Products(){
+    return (
+    <>
+    <h1>Produtos</h1>
+    <Link href="/">Inicio</Link>
+    </>
+    )
+
+}
+
+
